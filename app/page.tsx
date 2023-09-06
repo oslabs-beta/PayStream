18318c7ba5087df24f9c313879be2c8a05ce8719
Liam <lhodges3@binghamton.edu>
2023-09-05 22:11:10 -0400

    stripe updats

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import React from "react";
+import StripeContainer from '@/components/StripeContainer';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Bill bot baggins
+      <StripeContainer/>
     </main>
   )
 }
---
+++ components/StripeContainer.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import axios from "axios";
+import { useState, useEffect, use } from "react";
+import InvoiceButton from '@/components/InvoiceButton';
+
+export default function StripeContainer() {
+
+    const [invoice, setInvoice] = useState();
+
+    const [id, setId] = useState([]); // need to discuss when this is set (getting id from link sent from salesforce?)
+
+    useEffect(() => {
+        fetchInvoice(id)
+    },[])
+
+    const fetchInvoice = async (id: any) => {
+        const {data} : any = await axios.post('/api/invoice', {
+            id: id
+        },
+        {
+            headers: {
+                "Content-Type" : "application/json"
+            }
+        });
+        console.log('data back from fetch invoice is: ', data)
+        setInvoice(data)
+    }
+    
+    
+      return (
+        <section className="w-full">
+            {invoice && 
+                <InvoiceButton id = {invoice.id} invoice = {invoice}/>
+            }
+        </section>
+      );
+}
