0d9c4ac9a92d39c7719421aa73873a20a2e58885
Chandler <lcchrty@gmail.com>
2023-09-06 20:06:44 -0700

    establish new branch for integrating salesforce and stripe

--- app/api/salesforce-webhook/route.ts
+++
@@ -1,1 +0,0 @@
-// this will be a webhook? alternatgely, we can put an sf api call in useEffect and then store that data in redis
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
 "use client";
-import axios from "axios";
-import { redisConnect } from "../lib/redis";
-<<<<<<< HEAD
-import PaymentsDisplay from "./payments/PaymentsDisplay";;
-import React from "react";
-import StripeContainer from '../components/StripeContainer';
-=======
+
 import PaymentsDisplay from "./payments/page";
->>>>>>> cc/redis-salesforce-connect
+import React from "react";
+import StripeContainer from "../components/StripeContainer";
 
 export default function Home() {
   return (
@@ -16,8 +11,8 @@ export default function Home() {
         {/* conditionally render Payments display */}
         {/* Added "paymentsdisplay" in anticipation of component - button is in here to make API call, you can move this wherever */}
         <PaymentsDisplay />
-        <StripeContainer/>
-    </main>
+        <StripeContainer />
+      </main>
     </>
   );
 }
