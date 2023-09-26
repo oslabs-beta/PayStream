c4f9f3b9d06d1909e14acdaddfe57f383127623e
Chandler <lcchrty@gmail.com>
2023-09-25 18:16:54 -0700

    merging updates

--- components/StripeContainer.tsx
+++ components/StripeContainer.tsx
@@ -1,40 +1,39 @@
-"use client"
+'use client';
 
-import axios from "axios";
-import React from "react";
-import { useState, useEffect, use } from "react";
+import axios from 'axios';
+import React from 'react';
+import { useState, useEffect, use } from 'react';
 import InvoiceButton from './InvoiceButton';
 
 export default function StripeContainer() {
+  const [invoice, setInvoice] = useState();
 
-    const [invoice, setInvoice] = useState();
+  const [id, setId] = useState([]); // need to discuss when this is set (getting id from link sent from salesforce?)
 
-    const [id, setId] = useState([]); // need to discuss when this is set (getting id from link sent from salesforce?)
+  // on page load, fetch stripe invoice data to store as props on button
+  useEffect(() => {
+    fetchInvoice(id);
+  }, []);
 
-		// on page load, fetch stripe invoice data to store as props on button
-    useEffect(() => {
-        fetchInvoice(id)
-    },[])
-
-    const fetchInvoice = async (id: any) => {
-        const {data} : any = await axios.post('/api/invoice', {
-            id: id
+  const fetchInvoice = async (id: any) => {
+    const { data }: any = await axios.post(
+      '/api/invoice',
+      {
+        id: id,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
         },
-        {
-            headers: {
-                "Content-Type" : "application/json"
-            }
-        });
-        console.log('data back from fetch invoice is: ', data)
-        setInvoice(data)
-    }
-    
-    
-      return (
-        <section className="w-full">
-            {invoice && 
-                <InvoiceButton id = {invoice.id} invoice = {invoice}/>
-            }
-        </section>
-      );
+      }
+    );
+    console.log('data back from fetch invoice is: ', data);
+    setInvoice(data);
+  };
+
+  return (
+    <section className='w-full'>
+      {invoice && <InvoiceButton id={invoice.id} invoice={invoice} />}
+    </section>
+  );
 }
