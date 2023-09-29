fb5091f21c78043ec8aca51b32deda6a09ada335
Robert Hoover <peelintaters85@gmail.com>
2023-09-28 19:04:32 -0700

    Removes some of the any types and replaces them with appropriate typing. Also deleted an unnecessary route that was for Next Auth.

--- app/api/invoice/route.ts
+++ app/api/invoice/route.ts
@@ -13,11 +13,12 @@ import { NextRequest, NextResponse } from "next/server";
 // get invoice data once link is clicked (unsure at the moment what data is sent along with link)
 const config: Stripe.StripeConfig = {
 	apiVersion: "2023-08-16",
+	typescript: true,
 }
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
 
-	const data = await req.json()
+	const { invoiceId }: { invoiceId: string } = await req.json()
 
 	const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, config);
 	//const invoiceId = await request.json();
@@ -25,8 +26,8 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
 	 * want the invoice ID to come from salesforce data OR req params
 	 * currently, hard coded to reference existing ID
 	 */
-	const invoiceId = "in_1NlzoQHT7cVdV2e0To8zJ080"
-	const invoice = await stripe.invoices.retrieve(data.invoiceId);
+	// The invoiceId from the req should be the ID in the URL sent to the client
+	const invoice = await stripe.invoices.retrieve(invoiceId);
 	// console.log('invoice info from getting invoice data is: ', invoice)
 
 	return NextResponse.json(invoice)
--- components/InvoiceDisplay.tsx
+++ components/InvoiceDisplay.tsx
@@ -5,7 +5,7 @@ export default function InvoiceDisplay() {
   return (
     <main className='flex h-full w-full flex-col items-center justify-center space-y-4'>
       <h1 className='flex w-1/2 items-start text-2xl font-bold'>Invoice</h1>
-      <section className='flex w-1/2 flex-col space-y-8 rounded-lg bg-gray-500/10 p-10 text-white/80'>
+      <section className='flex w-1/2 flex-col space-y-8 rounded-lg bg-black/80 p-10 text-white/80'>
         <StripeContainer />
       </section>
     </main>
