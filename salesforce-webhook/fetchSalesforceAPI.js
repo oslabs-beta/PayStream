e3304869b02a412a120a601eb4abf7ad8b4c0849
chandler <lcchrty@gmail.com>
2023-10-14 14:10:37 -0700

    routes to retrieve opp type and enter switch statement only for appropriate record type/payments functioning

--- salesforce-webhook/fetchSalesforceAPI.js
+++ salesforce-webhook/fetchSalesforceAPI.js
@@ -6,30 +6,79 @@ import axios from "axios";
  */
 
 /**
- * get opp record Id from payment record ID
+ * GET opportunity record type from salesforce based on record number received from update
+ * graphQL api call to salesforce for opportunity record type
+ * @param string - invoice id from data change capture event
+ * @return string - opportunity record type
  */
-const getOppRecordId = async (id) => {
+export const getOppRecordId = async (id) => {
   let data = JSON.stringify({
     query: `query payments ($Id: ID) {
-  uiapi {
-    query {
-      npe01__OppPayment__c (where: { Id: { eq: $Id } }) {
-        edges {
-          node {
-            Id
-            Opportunity_Account_Name__c {
-                value
-            }
-            npe01__Opportunity__c {
-                value
-            }
-          }
-        }
-      }
-    }
-  }
-}`,
+		uiapi {
+			query {
+				npe01__OppPayment__c (where: { Id: { eq: $Id } }) {
+					edges {
+						node {
+							Id
+							Opportunity_Account_Name__c {
+									value
+							}
+							npe01__Opportunity__c {
+									value
+							}
+						}
+					}
+				}
+			}
+		}
+	}`,
     variables: { Id: id },
+  });
+  let config = {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: "https://escsocal--lc001.sandbox.my.salesforce.com/services/data/v58.0/graphql",
+    headers: {
+      "X-Chatter-Entity-Encoding": "false",
+      "Content-Type": "application/json",
+      Authorization: process.env.SALESFORCE_TOKEN,
+      Cookie: process.env.SALESFORCE_COOKIE_AUTH,
+    },
+    data: data,
+  };
+
+  const fetchOppId = await axios.request(config);
+  const oppId =
+    fetchOppId.data.data.uiapi.query.npe01__OppPayment__c.edges[0].node
+      .npe01__Opportunity__c.value;
+
+  return oppId;
+};
+
+export const retreiveOppType = async (id) => {
+  const fetchOppId = await getOppRecordId(id);
+
+  let data = JSON.stringify({
+    query: `query payments ($Id: ID) {
+		uiapi {
+			query {
+				Opportunity (where: { Id: { eq: $Id } }) {
+					edges {
+						node {
+							Id
+							Name {
+									value
+							}
+							Type {
+									value
+							}
+						}
+					}
+				}
+			}
+		}
+	}`,
+    variables: { Id: fetchOppId },
   });
 
   let config = {
@@ -44,37 +93,11 @@ const getOppRecordId = async (id) => {
     },
     data: data,
   };
-  axios
-    .request(config)
-    .then((response) => {
-      console.log(JSON.stringify(response.data.uiapi));
-      return;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
 
-/**
- * GET opportunity record type from salesforce based on record number received from update
- * graphQL api call to salesforce for opportunity record type
- * @param string - invoice id from data change capture event
- * @return string - opportunity record type
- */
-const getOppRecordType = (id) => {
-  // const fetchOppId = await getOppRecordId(id);
-
-  // graphQL schemas passed as "data" property on axios request config
-  const oppIdData = {};
-  const oppIdConfig = {};
-
-  // const data = await axios.request(oppIdConfig);
-  console.log("getOppRecord type() record id: ", id);
-  const oppId = {
-    type: "opportunity",
-    id: id,
-  };
-  return oppId;
+  const fetchOppType = await axios.request(config);
+  const oppType =
+    fetchOppType.data.data.uiapi.query.Opportunity.edges[0].node.Type.value;
+  return oppType;
 };
 
 /**
@@ -93,5 +116,3 @@ const getOppRecordType = (id) => {
 /**
  * DELETE invoice in stripe
  */
-
-export default getOppRecordType;
--- salesforce-webhook/salesforce-pub-sub-api.js
+++ salesforce-webhook/salesforce-pub-sub-api.js
@@ -1,6 +1,6 @@
 import PubSubApiClient from "salesforce-pubsub-api-client";
 import dotenv from "dotenv";
-import getOppRecordType from "./fetchSalesforceAPI.js";
+import { getOppRecordId, retreiveOppType } from "./fetchSalesforceAPI.js";
 // import { getOppRecordType } from "./controller";
 dotenv.config();
 // require("dotenv").config
@@ -11,6 +11,23 @@ const {
   SALESFORCE_USERNAME,
   SALESFORCE_ORG_ID,
 } = process.env;
+
+// List of opportunity record types from SAlesforce, would like to find a way to have this sent to the app, not manually updated
+const recordTypes = [
+  "SP",
+  "BD",
+  "FD",
+  "LC",
+  "RF",
+  "OD",
+  "FOC",
+  "OA",
+  "CA",
+  "HR",
+  "Customized",
+  "SM",
+  "CC",
+];
 
 const salesforceController = {};
 
@@ -39,76 +56,70 @@ salesforceController.run = async () => {
      * identify change fields
      * if it matches this criteria, it should create a an invoice in stripe
      */
-    eventEmitter.on("data", (event) => {
+    eventEmitter.on("data", async (event) => {
       console.log(
         `Handling ${event.payload.ChangeEventHeader.entityName} change event ${event.replayId}`
       );
-      console.log(event, null, 2);
-      console.log(
-        "changed fields: ",
-        event.payload.ChangeEventHeader.changedFields
-      );
-      console.log("payment type: ", JSON.stringify(event.payload.For_Chart__c));
-      console.log(
-        "payment amount: ",
-        JSON.stringify(event.payload.npe01__Payment_Amount__c)
-      );
+      // console.log(event, null, 2);
+      // console.log(
+      //   "changed fields: ",
+      //   event.payload.ChangeEventHeader.changedFields
+      // );
 
       // initialize variable to payment record ID
       const recordId = event.payload.ChangeEventHeader.recordIds[0];
-      console.log("PUBSUB APPI recordId: ", recordId);
 
-      /**
-       * assign the evaluated result of invoking getOppRecordType function to id variable
-       */
-      const id = getOppRecordType(recordId);
+      // initialize oppType variable to the evaluated result of retrieveOppType function passing in recordId
+      const oppType = await retreiveOppType(recordId);
 
-      /**
-       * switch statement to handle cases based on "changeType" of event
-       */
-      switch (event.payload.ChangeEventHeader.changeType) {
+      // if the opp type that corresponds to the updated payment record is in our record types array AND the payment type is cost to client; the switch statement should run
+      if (recordTypes.includes(oppType)) {
         /**
-         *  if changeType is "create" need to store all collected fields amount, invoice due date, name/invoice number, opportunity id,
+         * switch statement to handle cases based on "changeType" of event
          */
-        case "CREATE": {
-          console.log(
-            "CREATE case changeType: ",
-            event.payload.ChangeEventHeader.changeType
-          );
-          // receive invoice record Id AND opp record Id
-          const paymentType = event.payload.For_Chart__c;
-          const paymentAmount = event.payload.npe01__Payment_Amount__c;
-          console.log("payment amount: ", paymentAmount.double);
-          console.log("payment type: ", paymentType.string);
-          console.log("CREATE id: ", id);
-          // const oppType = getOppRecordType(id);
-          // if (oppType === "Consulting Engagment" || oppType === "Institute") {
-          // 	// create invoice in stripe - create function in controller module
-          // }
-          break;
-        }
-        /**
-         * if changeType is "update" /**
-         * use changed fields to identify which properties to data capture and then find relevant invoice in stripe to update
-         */
-        case "UPDATE": {
-          console.log(
-            "UPDATE case changeType: ",
-            event.payload.ChangeEventHeader.changeType
-          );
-          // only have invoice record id
-          console.log("UPDATE id: ", id);
-          break;
-        }
-        case "DELETE": {
-          console.log(
-            "DELETE case changeType: ",
-            event.payload.ChangeEventHeader.changeType
-          );
-          break;
-        }
-        default: {
-          console.log("default case hit: ", JSON.stringify(event, null, 2));
+        switch (event.payload.ChangeEventHeader.changeType) {
+          /**
+           *  if changeType is "create" need to store all collected fields amount, invoice due date, name/invoice number, opportunity id,
+           */
+          case "CREATE": {
+            console.log(
+              "CREATE case changeType: ",
+              event.payload.ChangeEventHeader.changeType
+            );
+            // receive invoice record Id AND opp record Id
+            const paymentType = event.payload.For_Chart__c;
+            const paymentAmount = event.payload.npe01__Payment_Amount__c;
+            console.log("payment amount: ", paymentAmount.double);
+            console.log("payment type: ", paymentType.string);
+            console.log("CREATE id: ", oppId);
+            // const oppType = getOppRecordType(id);
+            // if (oppType === "Consulting Engagment" || oppType === "Institute") {
+            // 	// create invoice in stripe - create function in controller module
+            // }
+            break;
+          }
+          /**
+           * if changeType is "update" /**
+           * use changed fields to identify which properties to data capture and then find relevant invoice in stripe to update
+           */
+          case "UPDATE": {
+            console.log(
+              "UPDATE case changeType: ",
+              event.payload.ChangeEventHeader.changeType
+            );
+            // only have invoice record id
+            break;
+          }
+          case "DELETE": {
+            console.log(
+              "DELETE case changeType: ",
+              event.payload.ChangeEventHeader.changeType
+            );
+            break;
+          }
+          default: {
+            console.log("default case hit: ", JSON.stringify(event, null, 2));
+          }
         }
       }
     });
