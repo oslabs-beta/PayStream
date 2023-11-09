db7153549ff17d409acd882efeb5b29b49eea663
Robert Hoover <peelintaters85@gmail.com>
2023-11-08 18:34:14 -0700

    Deleted unused components

--- bill-bot-baggins/components/index.ts
+++ bill-bot-baggins/components/index.ts
@@ -1,13 +1,10 @@
 import Navbar from './Navbar';
 import CustButton from './CustButton';
-import Header from './Header';
 import Footer from './Footer';
-import { SearchFilter } from './SearchFilter';
+
 
 export {
     Navbar,
-    Header,
     CustButton,
     Footer,
-    SearchFilter,
 };
