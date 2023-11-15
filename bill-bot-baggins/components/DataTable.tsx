930592a78619a0141b202ee8eaf8c57d1db11db2
Robert Hoover <peelintaters85@gmail.com>
2023-11-14 20:08:40 -0700

    Fixed pagination issue in the DataTable.

--- bill-bot-baggins/components/DataTable.tsx
+++ bill-bot-baggins/components/DataTable.tsx
@@ -3,6 +3,7 @@
 import {
   ColumnDef,
   ColumnFiltersState,
+  PaginationState,
   SortingState,
   flexRender,
   getCoreRowModel,
@@ -22,32 +23,47 @@ import {
 } from '@/components/ui/table';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 type DataTableProps<TData, TValue> = {
   columns: ColumnDef<TData, TValue>[];
   data: TData[];
 };
 
-export function DataTable<TData, TValue>({
+export default function DataTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
   const [sorting, setSorting] = useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
 
+  const [{ pageIndex, pageSize }, setPagination] = useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: 8,
+  });
+
+  const pagination = useMemo(
+    () => ({
+      pageIndex,
+      pageSize,
+    }),
+    [pageIndex, pageSize]
+  );
+
   const table = useReactTable({
     data,
     columns,
+    onPaginationChange: setPagination,
     getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
     onSortingChange: setSorting,
+    getPaginationRowModel: getPaginationRowModel(),
     getSortedRowModel: getSortedRowModel(),
     onColumnFiltersChange: setColumnFilters,
     getFilteredRowModel: getFilteredRowModel(),
     state: {
       sorting,
       columnFilters,
+      pagination,
     },
   });
 
@@ -144,7 +160,6 @@ export function DataTable<TData, TValue>({
       </div>
       <div className='flex items-center justify-end space-x-2 py-4'>
         <Button
-          variant='outline'
           size='sm'
           onClick={() => table.previousPage()}
           disabled={!table.getCanPreviousPage()}
@@ -152,13 +167,19 @@ export function DataTable<TData, TValue>({
           Previous
         </Button>
         <Button
-          variant='outline'
           size='sm'
           onClick={() => table.nextPage()}
-          // disabled={!table.getCanNextPage()}
+          disabled={!table.getCanNextPage()}
         >
           Next
         </Button>
+        <span className='flex items-center gap-1 text-sm'>
+          <div>Page</div>
+          <strong>
+            {table.getState().pagination.pageIndex + 1} of{' '}
+            {table.getPageCount()}
+          </strong>
+        </span>
       </div>
     </>
   );
