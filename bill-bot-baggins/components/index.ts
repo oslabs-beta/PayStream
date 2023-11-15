63aca6605e1c0963f6a9fdfb6444d255ad232916
Robert Hoover <peelintaters85@gmail.com>
2023-11-14 20:27:45 -0700

    Updated the admin page to be more modular, and separated more of the logic into the utils folder.

--- bill-bot-baggins/components/index.ts
+++ bill-bot-baggins/components/index.ts
@@ -1,10 +1,18 @@
 import Navbar from './Navbar';
 import CustButton from './CustButton';
 import Footer from './Footer';
+import Overview from './overview';
+import { RecentSales } from './RecentSales';
+import { columns }from '@/components/Columns';
+import DataTable from './DataTable';
 
 
 export {
     Navbar,
     CustButton,
     Footer,
+    Overview,
+    RecentSales,
+    columns,
+    DataTable,
 };
